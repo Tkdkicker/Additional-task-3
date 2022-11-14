0c2be9149dff877e2fed52a628e3086ee6833035
gemma <gemma@DESKTOP-2Q2G917>
2022-11-14 10:42:06 +0000

    Add project files.

---
+++ Additional task 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Additional_task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a day of the week");
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "Monday":
+                    Console.WriteLine("You chose " + userInput);
+                    break;
+                case "Tuesday":
+                    Console.WriteLine("You chose " + userInput);
+                    break;
+                case "Wednesday":
+                    Console.WriteLine("You chose " + userInput);
+                    break;
+                case "Thursday":
+                    Console.WriteLine("You chose " + userInput);
+                    break;
+                case "Friday":
+                    Console.WriteLine("You chose " + userInput);
+                    break;
+                case "Saturday":
+                    Console.WriteLine("You chose " + userInput);
+                    break;
+                case "Sunday":
+                    Console.WriteLine("You chose " + userInput);
+                    break;
+            }
+        }
+    }
+}
